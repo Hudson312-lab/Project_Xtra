0b64615d8643d2318adc8f6671dfdf11d12f34b5
Hudson Flynn <hudsonflynn63@gmail.com>
2024-06-29 14:53:40 +0500

    "test"

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,19 +1,22 @@
 
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <></>
-    // <nav className="bg-gradient-to-r from-indigo-500 via-indigo-600 to-indigo-700 p-4">
-    //   <div className="container mx-auto flex justify-between items-center">
-    //     <div className="text-white text-lg font-bold">Alpha Investments</div>
-    //     {/* <div className="space-x-4">
-    //       <Link to="/" className="text-white">Home</Link>
-    //       <Link to="/about" className="text-white">About</Link>
-    //       <Link to="/contact" className="text-white">Contact</Link>
-    //     </div> */}
-    //   </div>
-    // </nav>
+    <>
+     <nav className="bg-gradient-to-r from-indigo-500 via-indigo-600 to-indigo-700 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+      <div className="text-white text-lg font-bold">Alpha Investments</div>
+<div className="space-x-4">
+     <Link to="/" className="text-white">Home</Link>
+   {/* <Link to="/about" className="text-white">About</Link>
+   <Link to="/contact" className="text-white">Contact</Link> */}
+     </div>
+      </div>
+    </nav>
+
+
+    </>
   );
 };
 
