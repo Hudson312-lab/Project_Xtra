484490f9d7d2bad32b2d8ec8ef94633e2dbf27d4
hudson <hudsonflynn63@gmail.com>
2024-06-29 13:18:08 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from './pages/Home';
+// import About from './pages/About';
+import Navbar from './componenets/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/componenets/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-500 p-4">
+      <ul className="flex space-x-4">
+        <li>
+          <Link to="/" className="text-white">Home</Link>
+        </li>
+        <li>
+          <Link to="/about" className="text-white">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
