c8e80a484696e5a3a2b772e30968713041b6a05b
Hudson Flynn <hudsonflynn63@gmail.com>
2024-06-29 18:45:01 +0500

    "a"

--- src/pages/ActivateAccount.jsx
+++ src/pages/ActivateAccount.jsx
@@ -4,13 +4,13 @@ const ActivateAccount = () => {
   const address = "bnb1u7tcrsffj4y7v3ysh4rvsz5q6hxjsgfhvllhxz";
 
   return (
-    <div className="max-w-lg mx-auto p-8 bg-white rounded-lg shadow-lg">
-      <h2 className="text-2xl font-bold mb-6 text-gray-800">Activate your account</h2>
-      <p className="text-gray-700 mb-4">
+    <div className="max-w-lg min-h-screen mx-auto p-8 bg-white rounded-lg">
+      <h2 className="text-2xl font-bold mb-6 text-indigo-700">Activate your account</h2>
+      <p className="text-gray-700 mb-4 font-medium">
         Deposit $10 from your Binance to this address using Binance BEP20 for account activation.
       </p>
       <div className="mb-4">
-        <label className="block text-gray-700 font-bold mb-2">Address via direct:</label>
+        <label className="block  font-bold mb-2 text-indigo-700"> Binance BEP20 Address :</label>
         <textarea
           readOnly
           className="w-full p-3 border border-gray-300 rounded-lg bg-gray-100 focus:outline-none"
@@ -19,7 +19,7 @@ const ActivateAccount = () => {
         />
       </div>
       <div className="mb-4">
-        <label className="block text-gray-700 font-bold mb-2">Submit your TRXID and Screenshot:</label>
+        <label className="block text-indigo-700 font-bold mb-2">Submit your TRXID and Screenshot:</label>
         <p className="w-full p-3 border border-gray-300 rounded-lg bg-gray-100">
           Send your TRXID and Screenshot to{' '}
           <a href="mailto:alphainvestmentone@hotmail.com" className="text-indigo-600 underline">
@@ -27,8 +27,8 @@ const ActivateAccount = () => {
           </a>
         </p>
       </div>
-      <p className="text-gray-700">
-        Your profile will be activated within 24 hours.
+      <p className="text-gray-700 mt-6 font-normal">
+        Your profile will be activated within 24 hours, in case of any issue, feel free to mail us. We are here to serve you 24/7.
       </p>
     </div>
   );
