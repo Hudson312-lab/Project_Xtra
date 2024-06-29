5e0794fca24dcb99957de7b82aec0bcff4ee5f8e
Hudson Flynn <hudsonflynn63@gmail.com>
2024-06-29 14:50:40 +0500

    Test commit to verify SSH configuration

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -3,16 +3,17 @@
 
 const Navbar = () => {
   return (
-    <nav className="bg-gradient-to-r from-indigo-500 via-indigo-600 to-indigo-700 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-lg font-bold">Alpha Investments</div>
-        {/* <div className="space-x-4">
-          <Link to="/" className="text-white">Home</Link>
-          <Link to="/about" className="text-white">About</Link>
-          <Link to="/contact" className="text-white">Contact</Link>
-        </div> */}
-      </div>
-    </nav>
+    <></>
+    // <nav className="bg-gradient-to-r from-indigo-500 via-indigo-600 to-indigo-700 p-4">
+    //   <div className="container mx-auto flex justify-between items-center">
+    //     <div className="text-white text-lg font-bold">Alpha Investments</div>
+    //     {/* <div className="space-x-4">
+    //       <Link to="/" className="text-white">Home</Link>
+    //       <Link to="/about" className="text-white">About</Link>
+    //       <Link to="/contact" className="text-white">Contact</Link>
+    //     </div> */}
+    //   </div>
+    // </nav>
   );
 };
 
