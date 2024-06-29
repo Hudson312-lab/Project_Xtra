194467c796d4729b67064b98165462e72bab4a55
Hudson Flynn <hudsonflynn63@gmail.com>
2024-06-30 01:44:10 +0500

    "m"

--- src/pages/ActivateAccount.jsx
+++ src/pages/ActivateAccount.jsx
@@ -1,6 +1,6 @@
 import { FaCopy, FaCheckCircle } from 'react-icons/fa';
 import { isMobile } from 'react-device-detect';
-import { useEffect } from 'react';
+import addressQR from "../assets/address.jpeg";
 
 const ActivateAccount = () => {
   const address = "TTsencRWSr3qioVVjb6BPQ4ACLGvgUuTqk";
@@ -10,15 +10,6 @@ const ActivateAccount = () => {
     alert('Address copied to clipboard!');
   };
 
-  useEffect(() => {
-    // Disable scrolling on component mount
-    document.body.style.overflow = 'hidden';
-
-    // Enable scrolling on component unmount
-    return () => {
-      document.body.style.overflow = 'auto';
-    };
-  }, []);
 
   if (!isMobile) {
     return (
@@ -31,13 +22,16 @@ const ActivateAccount = () => {
   }
 
   return (
-    <div className="max-w-lg min-h-screen mx-auto p-8  bg-gray-50 rounded-lg shadow-lg">
+    <div className="max-w-lg min-h-screen mx-auto p-8 bg-gray-50 rounded-lg shadow-lg">
       <h2 className="text-2xl font-extrabold mb-6 text-indigo-700 flex items-center">
         <FaCheckCircle className="mr-2" /> Activate your account
       </h2>
       <p className="text-gray-700 mb-4 font-medium">
-        Deposit <strong>10 USDT</strong> from your <strong>Binance</strong> to this address using  <strong>TRC20</strong> for account activation.
+        Deposit <strong>10 USDT</strong> from your <strong>Binance</strong> to this address using <strong>TRC20</strong> for account activation.
       </p>
+      <div className="mb-4">
+        <img src={addressQR} alt="Address QR Code" className="w-full h-auto rounded-lg shadow-md" />
+      </div>
       <div className="mb-4">
         <label className="block font-bold mb-2 text-indigo-700">TRC20 Address:</label>
         <div className="relative">
@@ -58,13 +52,11 @@ const ActivateAccount = () => {
       <div className="mb-4">
         <label className="block text-indigo-700 font-bold mb-2">Submit your Payment TRXID and Screenshot to:</label>
         <p className="w-full p-3 border border-gray-300 rounded-lg bg-gray-100 text-indigo-600 flex items-center font-bold text-lg">
-          
-             alphainvestmentsone@hotmail.com
-
+          alphainvestmentsone@hotmail.com
         </p>
       </div>
       <p className="text-gray-700 mt-6 font-normal">
-        Your profile will be activated within 24 hours, in case of any issue, feel free to mail us. We are here to serve you 24/7.
+        Your profile will be activated within 24 hours. In case of any issues, feel free to email us. We are here to serve you 24/7.
       </p>
     </div>
   );
