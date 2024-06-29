735a90c373949a612509216b9ccb5ed54e583d09
hudson <hudsonflynn63@gmail.com>
2024-06-29 13:23:18 +0500

    "test2"

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="bg-blue-500 p-4">
       <ul className="flex space-x-4">
         <li>
-          <Link to="/" className="text-white">Home</Link>
+          <Link to="/" className="text-white">Homecheck</Link>
         </li>
         <li>
           <Link to="/about" className="text-white">About</Link>
