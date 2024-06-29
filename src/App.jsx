208f3516ae84f8e89ab0d2afde92e764c6ec1d44
Hudson Flynn <hudsonflynn63@gmail.com>
2024-06-29 15:11:28 +0500

    "a"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './componenets/Navbar';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
