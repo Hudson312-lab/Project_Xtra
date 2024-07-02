d06e2311c5142d25f66bb0979f8ef3767b8966b8
Hudson Flynn <hudsonflynn63@gmail.com>
2024-07-02 15:01:49 +0500

    "a"

--- src/pages/Binance.jsx
+++ src/pages/Binance.jsx
@@ -62,11 +62,11 @@ const InstallBinancePage = () => {
 
   return (
     <div className="min-h-screen flex flex-col items-center p-4 bg-gray-100 overflow-hidden">
-      <h1 className="text-3xl mt-2 font-extrabold mb-4 text-yellow-700 text-center leading-snug border-2 rounded-md p-2 ">
+      <h1 className="text-3xl font-extrabold mb-4 text-yellow-700 text-center leading-snug border-2 rounded-md p-2 ">
         Install, Get Registered, and Start Using BINANCE
         <span className="block mt-2 text-2xl text-yellow-600">This investment and earning opportunity is for BINANCE users only !</span>
       </h1>
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-6">
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-4">
         <a 
           href="https://play.google.com/store/apps/details?id=com.binance.dev&hl=en&pli=1" 
           target="_blank" 
