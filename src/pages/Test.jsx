61f6dfbf70300be5e312830bbafd45840c200a53
Hudson Flynn <hudsonflynn63@gmail.com>
2024-07-05 00:04:17 +0500

    "a"

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -4,12 +4,17 @@ import data from "../assets/data.jpg";
 
 const Test = () => {
   
+  // Bronze below 10k
+  // Silver 10k-25k
+  // 25k-50k
+  // 50k-100k
+  // 500k
 
   return (
     <div className="min-h-screen bg-indigo-200 p-4 flex flex-col items-center">
       <div className="w-full max-w-md flex flex-col space-y-4">
         <div className="bg-gradient-to-b from-indigo-800 to-indigo-600 shadow-lg rounded-lg overflow-hidden p-4 flex flex-col items-center space-y-4">
-          <h2 className="text-3xl text-center font-bold bg-indigo-600 w-full p-2 rounded text-white">Dawood Sheikh</h2>
+          <h2 className="text-3xl text-center font-bold bg-indigo-600 w-full p-2 rounded text-white">Aamir Hussain</h2>
           <div className="w-full flex items-center ml-14">
             <div className="flex items-center space-x-4"> 
               <img
@@ -25,12 +30,9 @@ const Test = () => {
                   </div>
                   <div className="flex items-center space-x-2">
                     <h3 className="text-lg font-semibold text-white">Level:</h3>
-                    <p className="text-lg font-bold text-yellow-400">Silver</p>
+                    <p className="text-lg font-bold text-yellow-400">Golden</p>
                   </div>
-                  {/* <div className="flex items-center space-x-2">
-                    <h3 className="text-lg font-semibold text-white">UID:</h3>
-                    <p className="text-lg font-bold text-red-300">UN178295</p>
-                  </div> */}
+
                   <p className="text-base text-white flex items-center mt-2">
                     <FaCheckCircle className="w-4 h-4 text-green-500 mr-1" />
                     Verified
@@ -44,11 +46,11 @@ const Test = () => {
         {/* Profile Details */}
         <div className="w-full grid font-bold grid-cols-1 sm:grid-cols-2 gap-4">
           {[
-            { icon: FaMoneyBillWave, title: "Investment", value: "$17000", color: "text-yellow-600", bgColor: "bg-gray-100" },
-            { icon: FaChartLine, title: "ROI", value: "$7800", color: "text-green-500", bgColor: "bg-gray-50" },
-            { icon: FaDollarSign, title: "Affiliate Earnings", value: "$3500", color: "text-purple-600", bgColor: "bg-gray-100" },
-            { icon: FaDollarSign, title: "Total Income", value: "$11300", color: "text-blue-600", bgColor: "bg-gray-100" },
-            { icon: FaPiggyBank, title: "Withdrawals", value: "$7480", color: "text-pink-600", bgColor: "bg-gray-100" },
+            { icon: FaMoneyBillWave, title: "Investment", value: "$28600", color: "text-yellow-600", bgColor: "bg-gray-100" },
+            { icon: FaChartLine, title: "ROI", value: "$37000", color: "text-green-500", bgColor: "bg-gray-50" },
+            { icon: FaDollarSign, title: "Affiliate Earnings", value: "$23040", color: "text-purple-600", bgColor: "bg-gray-100" },
+            { icon: FaDollarSign, title: "Total Income", value: "$60040", color: "text-blue-600", bgColor: "bg-gray-100" },
+            { icon: FaPiggyBank, title: "Withdrawals", value: "$57820", color: "text-pink-600", bgColor: "bg-gray-100" },
           ].map((item, index) => (
             <div key={index} className={`shadow-lg rounded-lg overflow-hidden p-4 flex items-center ${item.bgColor}`}>
               <item.icon className={`w-8 h-8 mr-4 ${item.color}`} />
